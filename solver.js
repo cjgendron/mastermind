729f8fbf49843ffeb0c019e3489527a008b77dfb
Cathleen Gendron <gendron@mit.edu>
2014-07-06 19:54:39 -0400

    Added variables for simpler parameter changes

--- solver.js
+++ solver.js
@@ -1,4 +1,6 @@
 var Mastermind = function() {
+    var NUM_SPACES = 5
+    var NUM_COLORS = 8
     var AnalyzeRow = function(values) {
             var pattern_temp = guesses[guesses.length - 1].slice(0),
                 results = { reds: 0, whites: 0 };
@@ -25,13 +27,13 @@ var Mastermind = function() {
 
             return results;
         },
-        colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black' ],
+        colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black' ].slice(0, NUM_COLORS),
         current_try,
         dropdown,
         GeneratePattern = function() {
             var pattern = [];
 
-            for(var i = 0; i < 5; i++) {
+            for(var i = 0; i < NUM_SPACES; i++) {
                 pattern[i] = GetRandomColorInt().toString();
             }
 
@@ -44,7 +46,7 @@ var Mastermind = function() {
             return Math.floor(Math.random() * 8);
         },
         guess,
-        guesses = [ [ 0, 1, 2, 3, 4 ] ],
+        guesses = [ [ 0, 1, 2, 3, 4, 5, 6, 7 ].slice(0, NUM_SPACES) ],
         Init = function() {
             InitDropdown();
             InitGuess();
@@ -83,16 +85,21 @@ var Mastermind = function() {
             });
         },
         InitPossibleAnswers = function() {
-            for(var i = 0; i < 32768; i++) {
-                var possible_answer = ("0000"+i.toString(8)).slice(-5);
+            for(var i = 0; i < Math.pow(NUM_COLORS, NUM_SPACES); i++) {
+                var possible_answer = ("0000"+i.toString(NUM_COLORS)).slice(-NUM_SPACES);
 
                 possible_answers.push(possible_answer);
             }
         },
         possible_answers = [],
         RenderGuess = function() {
+            var tds = ""
+            for(var i = 0; i < NUM_SPACES; i++) {
+                tds = tds.concat('<td></td>')
+            }
+
             table_body = $('tbody');
-            table_body.append(guess);
+            table_body.append('<tr class="guess">' + tds + '</tr>');
 
             var row = $('tr:last-child');
             row.find('td').each(function(index) {
@@ -105,14 +112,15 @@ var Mastermind = function() {
             row.append(dropdown);
         },
         results = [ 'red', 'white' ],
-        results_options = [ 0, 1, 2, 3, 4, 5 ],
+        results_options = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ].slice(0, NUM_SPACES + 1),
+        // tds = [0,0,0,0,0,0,0,0,0,0].slice(0, NUM_SPACES - 1)
         status,
         table_body,
         table_cell,
         table_row,
         templates = {
             dropdown: '{{#results}}<td><select class="results {{.}}">{{#results_options}}<option>{{.}}</option>{{/results_options}}</select></td>{{/results}}',
-            guess: '<tr class="guess"><td></td><td></td><td></td><td></td><td></td></tr>'
+            guess: '<tr class="guess"></tr>'
         };
 
     Init();
